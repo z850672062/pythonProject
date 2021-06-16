8a50794ca28d9bf0f9afb8539b3b9bee1246a1a6
z850672062 <850672062@qq.com>
2021-06-16 17:08:00 +0800

    2021-6-16 17:07:50

--- babys/commodity/urls.py
+++ babys/commodity/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     #<int:id>为变量，他以整数型表示，变量id对应商品信息表的主键id，改变变量id的数值可以查看不同商品的详细介绍
     #路由地址末端设置.html 是一种伪静态技术，此外还可以为变量id设置终止符。不然随便输入无限长的数字也能正常访问
     path('/detail.<int:id>.html', detailView, name='detail'),
+    path('/collect.html',collectView,name='collect'),
 
 
 
--- babys/commodity/views.py
+++ babys/commodity/views.py
@@ -6,12 +6,18 @@ from .models import *
 from django.http import JsonResponse
 from django.db.models import F
 
+# 路由detail设置了路由变量id，所以相应的视图函数必须设置相应的函数参数。,在html文件中也要写入id参数
+
 def detailView(request, id):
     title = '商品介绍'
     classContent = 'datails'
+    # 商品展示
     commoditys = CommodityInfos.objects.filter(id=id).first()
+    # 热销推荐 items查询CommodityInfos前五名销量最高的商品信息，在查询过程中可以使用exclude将当前商品id=id信息排除
     items = CommodityInfos.objects.exclude(id=id).order_by('-sold')[:5]
+    #likesList变量是当前用户与django的会话连接，session会话，
     likesList = request.session.get('likes', [])
+    # 收藏按钮 likes判断变量likesList是否含有当前商品的主键id，如果当前id已存在变量likesList中，那么说明用户已经收藏了当前商品
     likes = True if id in likesList else False
     return render(request, 'details.html', locals())
 
@@ -58,14 +64,27 @@ def commodityView(request):
     return render(request, 'commodity.html', locals())
 
 
-
+# 通过读写用户的会话session数据来记录商品的收藏情况，具体的业务逻辑说明如下
 def collectView(request):
+    # 首先从请求对象request获取请求参数id，并赋值给变量id，它代表当前商品的主键id
     id = request.GET.get('id', '')
+    # 然后设置响应内容result，并以字典格式标识
     result = {"result": "已收藏"}
+    # 最后从请求对象request获取会话session数据likes，如果存在数据likes，则赋值给变量likes，否则变量likes设置空列表
     likes = request.session.get('likes', [])
+    # 如果变量id不为空，并且变量id不在变量likes里面（likes是列表格式）
+    # 那么说明当前商品尚未被当前用户加入收藏，程序将执行商品收藏
     if id and not int(id) in likes:
         # 对商品的收藏数量执行自增加1
+        # 将变量id作为CommoInfos的查询条件，再有查询对象使用update（）和F（）方法实现
+        # 字段likes的自增加1操作；
         CommodityInfos.objects.filter(id=id).update(likes=F('likes')+1)
+        # 然后将响应内容result改为收藏过程
         result['result'] = "收藏成功"
+        # 最后将当前商品主键id写入会话session数据likes，标记当前商品已经被当前用户收藏了
         request.session['likes'] = likes + [int(id)]
+    #     视图函数返回值使用JsonResonse将变量result作为响应内容，JsonResponse能将Python的字典转换为JSON数据。
+    # 此外也可以用HttpResponse来实现，不过需要自行将字典转换为JSON数据
+    # 比如 HttpResponse(json.dumps(result),content_type='application/json'),首先使用JSON模块转换字典result，然后响应类型content_type要设为application/json
+
     return JsonResponse(result)
