7dcbd96a687da44e4e9a09082b8c2e9c2c4c920b
souikunn <850672062@qq.com>
2021-07-12 21:50:15 +0800

    2021-7-12 21:50:12

--- crossion的实验小作业/caishuzi.py
+++ crossion的实验小作业/caishuzi.py
@@ -13,8 +13,8 @@ Rev2021-7-8 09:30:01
 最后将结果保存在文件中（保存在game_one_user.txt)中
 Rev2021-7-12 14:32:56
 新增内容：
-txt文件中有历史记录（数据）的话，询问是否读取历史记录，
-没有数值，不询问直接开始游戏。
+txt文件是否存在，存在读取游戏
+不存在，初始化游戏
 
 """
 import random
@@ -25,10 +25,6 @@ def guess_number():
     # 猜数字数据字典
     guess_dict = {}
     player = input('请输入你的名字：')
-
-    # # 添加玩家名
-    # guess_dict['玩家'] = player
-
     # 记录次数
     times = 0
     # 记录最快猜中轮数列表
@@ -72,42 +68,90 @@ def guess_number():
             break
     # 添加key和value的数据
     guess_dict[player] = str(str(times) + ' ' + str(fast_count) + ' ' + str(all_count))
-    # print(guess_dict)
-
-    # # 添加总游戏次数
-    # guess_dict['总游戏次数'] = times
-    # # 添加总游戏轮数
-    # guess_dict['总游戏轮数'] = all_count
-    # # 添加最快猜中轮数
-    # guess_dict['最快猜中轮数'] = fast_count
-
     return guess_dict
 
 def init_file(guess_dict):
-    print(guess_dict)
-    with open('game_one_user.txt', 'w+', encoding='utf-8') as p:
+    # print(guess_dict)
+    with open('game_one_user.txt', 'w', encoding='utf-8') as p:
         # 循环遍历字典中的key和value值写入到txt文本
         for k in guess_dict:
             str2 = str(k + ' ' + str(guess_dict[k]))
             p.writelines(str2)
 
-def history_file(guess_dict):
+def history_file():
     with open('game_one_user.txt', 'r', encoding='utf-8') as p:
-        list2 = p.readlines()
-        print(list2)
+        list2 = (p.readlines())[0].split()
+    #     guess_dict = {}
+    #     guess_dict[list2[0]] = list2[1] + ' ' + list2[2] + ' ' + list2[3]
+    #
+    # print(guess_dict)
+    # 添加玩家名
+    player = list2[0]
+    # 记录次数
+    times = int(list2[1])
+    # 记录最快猜中轮数列表
+    fast_count = int(list2[2])
+    # 记录总游戏轮数
+    all_count = int(list2[3])
+    #
+    average1 = all_count/times
+
+    print('%s，你已经玩了%d次，最少%d轮猜出答案,平均%.2f轮猜出答案' % (player, times, fast_count, average1))
+    while True:
+        # 生成随机数1-100
+        random_num = random.randint(1, 100)
+        print(random_num)
+
+        print('废话不多说，咋们开始吧，请输入你想到的数字(1 - 100)：')
+        count = 0  # 记录循环内的轮数
+
+        while True:
+            guess_number = int(input())
+            count += 1
+            if guess_number > random_num:
+                print('哎呀，猜大了再试试')
+
+            elif guess_number < random_num:
+                print('哎呀，猜小了再试试')
+            else:
+                print('恭喜你猜对了，你真是神机妙算呀！，你只花了%d轮就猜出来了' % count)
+
+                break
+        # 如果游戏次数=0 或者 游戏轮数小于最小轮数就更新最小轮数= count
+        if times == 0 or count < fast_count:
+            fast_count = count
+        # 游戏总次数加1
+        times += 1
+        # 总轮数增加
+        all_count += count
+
+        # 一次游戏平均几轮猜中 =总游戏轮数/总游戏次数
+        average1 = all_count/times
+        print('%s，你已经玩了%d次，最少%d轮猜出答案,平均%.2f轮猜出答案' % (player,times,fast_count,average1))
+        judge = input('是否继续游戏？(输入y继续，其他退出)')
+        if judge != 'y':
+            print('退出游戏，欢迎下次再来！')
+            break
+
+    with open('game_one_user.txt', 'w', encoding='utf-8') as p:
+        p.write('%s %d %d %d' % (player,times,fast_count,all_count))
 
 
 if __name__ == '__main__':
     judge = pathlib.Path('game_one_user.txt').exists()
     # 判断文件是否存在，不存在生成，存在读档写入
+
     if judge != True:
         print('生成游戏数据文件中...')
+
         guess_dict = guess_number()
         init_file(guess_dict)
     else:
         print('读取游戏数据,继续上次游戏')
+        history_file()
 
-        history_file(guess_dict)
+
+
 
 
 
